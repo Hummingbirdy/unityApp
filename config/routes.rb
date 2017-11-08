4530320865400423edd1484d45e6ac503933cfb6
James Foster <JamesFoster3@my.unt.edu>
2017-11-08 13:28:50 -0600

    made style changes to projects page, styling text adding boxes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   resources :projects
   resources :groups
 
+ 
   get "/groups/add/:id", :to => "groups#add", :as => 'add_groups'
   get "/projects/add/:id", :to => "projects#add", :as => 'add_projects'
+
+ 
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
