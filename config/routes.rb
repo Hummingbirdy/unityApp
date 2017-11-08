e5aa3e7368b62c7e6818d2d30e8ced53cb2f1700
tlemaster <taylorlemaster@my.unt.edu>
2017-11-08 13:34:12 -0600

    add static pages for doc's and chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/show'
+
   resources :tasks
   resources :projects
   resources :groups
 
   get "/groups/add/:id", :to => "groups#add", :as => 'add_groups'
   get "/projects/add/:id", :to => "projects#add", :as => 'add_projects'
+  get '/pages/:page' => 'pages#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
