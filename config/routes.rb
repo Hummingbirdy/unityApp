23b553a16ae06d0b8726ac55bb0fcc9f5efee505
tlemaster <taylorlemaster@my.unt.edu>
2017-11-13 13:39:07 -0600

    Revert "devise"
    This reverts commit 19b5630204440d726b04d4b274e45c1338315963.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  root to: 'groups#index'
+  get 'pages/show'
 
   resources :tasks
   resources :projects
   resources :groups
 
-  get '/pages/:page' => 'pages#show'
-  get '/groups/add/:id', :to => 'groups#add', :as => 'add_groups'
-  get '/projects/add/:id', :to => 'projects#add', :as => 'add_projects'
+ 
+  get "/groups/add/:id", :to => "groups#add", :as => 'add_groups'
+  get "/projects/add/:id", :to => "projects#add", :as => 'add_projects'
+<<<<<<< HEAD
 
+ 
+
+
+=======
+  get '/pages/:page' => 'pages#show'
+>>>>>>> e5aa3e7368b62c7e6818d2d30e8ced53cb2f1700
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
